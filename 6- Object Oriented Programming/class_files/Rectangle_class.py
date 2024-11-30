83fb0835880b9b28afce261ff7cd6257d854ddd1
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-11-30 11:33:34 +0330

    Create Rectangle_class.py

---
+++ 6- Object Oriented Programming/class_files/Rectangle_class.py
@@ -0,0 +1,23 @@
+class Rectangle:
+    
+    def __init__(self, c, l, w):
+        self.c = c
+        self.l = l
+        self.w = w 
+    
+    def distance(self, other):
+        # Homework
+        pass 
+    
+    def is_square(self):
+        # Homework
+        pass 
+    
+    def has_common(self, other):
+        # Homework
+        pass 
+    
+    def draw(self):
+        # Homework
+        pass 
+        
