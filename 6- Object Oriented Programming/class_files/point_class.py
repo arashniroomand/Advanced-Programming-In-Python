bdd039685464f970e823a67036181c86b313b326
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2024-11-30 11:21:23 +0330

    Create point_class.py

---
+++ 6- Object Oriented Programming/class_files/point_class.py
@@ -0,0 +1,18 @@
+class point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, other):
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
+
+    def zone(self):
+        if self.x > 0 and self.y > 0:
+            return 1
+        elif self.x < 0 and self.y > 0:
+            return 2
+        elif self.x < 0 and self.y < 0:
+            return 3
+        elif self.x > 0 and self.y < 0:
+            return 4
